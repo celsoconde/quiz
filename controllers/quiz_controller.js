8787ab62cfdc4ea0e1dea177ca27e4f5f0b76925
Celso Conde <celso.conde.perez@gmail.com>
2015-07-06 12:36:50 +0200

    Indice tematico preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -40,7 +40,7 @@ exports.answer = function(req, res){
 // GET /quizes/new
 exports.new = function(req, res) {
   var quiz = models.Quiz.build( //Crea un objeto quiz
-    { pregunta: "Pregunta" , respuesta: "Respuesta"}
+    { pregunta: "Pregunta" , respuesta: "Respuesta", tema: "Tema"}
   );
   res.render('quizes/new', {quiz: quiz, errors: []});
 };
@@ -58,7 +58,7 @@ exports.create = function(req, res) {
     res.render('quizes/new',{quiz: quiz, errors: errores});
   } else {
     //guarda en DB los campos pregunta y respuesta de quiz
-    quiz.save({ fields: ["pregunta","respuesta"]}).then(function() {
+    quiz.save({ fields: ["pregunta","respuesta","tema"]}).then(function() {
       res.redirect('/quizes'); // Redireccion HTTP a lista de preguntas
       })
   }
@@ -74,6 +74,7 @@ exports.edit = function(req, res){
 exports.update = function(req, res){
   req.quiz.pregunta = req.body.quiz.pregunta;
   req.quiz.respuesta = req.body.quiz.respuesta;
+  req.quiz.tema = req.body.quiz.tema;
   var errors = req.quiz.validate();
   if (errors) {
     var i=0;
@@ -84,7 +85,7 @@ exports.update = function(req, res){
     res.render('quizes/edit',{quiz: req.quiz, errors: errores});
   } else {
     //guarda en DB los campos pregunta y respuesta de quiz
-    req.quiz.save({ fields: ["pregunta","respuesta"]}).then(function() {
+    req.quiz.save({ fields: ["pregunta","respuesta","tema"]}).then(function() {
       res.redirect('/quizes'); // Redireccion HTTP a lista de preguntas
       })
   }
--- models/quiz.js
+++ models/quiz.js
@@ -1,14 +1,17 @@
 //Definicion del modelo de Quiz
-
 module.exports = function (sequelize, DataTypes){
   return sequelize.define('Quiz',
             { pregunta: {
-              type: DataTypes.STRING,
-              validate: { notEmpty: { msg: "--> Falta Pregunta"}}
-            },
+                type: DataTypes.STRING,
+                validate: { notEmpty: { msg: "--> Falta Pregunta"}}
+              },
               respuesta: {
                 type : DataTypes.STRING,
                 validate: { notEmpty: { msg: "--> Falta Respuesta"}}
+              },
+              tema :{
+                type : DataTypes.STRING,
+                validate : { notEmpty : { msg : "--> Falta Tema"}}
               }
             });
 }
